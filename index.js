d7727549aa33aacf7bf9d3b85e2dae38da000863
Joakim Villo <villokodehode@gmail.com>
2022-12-05 23:46:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+//! Challange! use Js to make a button for each luke to make an image appear (appendchild/remove child?)
+
+const luke24Text = document.getElementById("luke24Text");
+
+console.log(luke24Text);
+//! kanskje må html flyttes til html.index?
+
+// const inputName = document.getElementById("inputName");
+// const nameButton = document.getElementById("nameButton");
+// const greet = document.getElementById("greet");
+
+// nameButton.addEventListener("click", greeting);
+
+// function greeting() {
+//   greet.textContent = `Hello there ${inputName.value}!`;
+// }
+
+// Function to reset image dimensions
+// function resetImg() {
+//   content2.style.width = "40%";
+//   content2.style.height = "auto";
+//   content2.style.transition = "width 0.5s ease";
+// }
+
+//* fungerer men er helt ubrukelig:
+
+// luke24Text.addEventListener("click", enlargeImg());
+
+// function enlargeImg() {
+//   console.log("click");
+//   luke24Text.style.fontSize = "1px";
+// }
+
+//*
+
+// <div class="luke jul">
+//   <label>
+//     <input type="checkbox" />
+//     <div class="cover"><p>God Jul<br>alle sammen!</p></div>
+//     <div class="inside">
+//     <img
+//   src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/merry-christmas-wishes-greeting-video-cats-ad-design-template-14114ed0a30602feca713c7187ab2158_screen.jpg?ts=1574593470"
+// />
+//     </div>
