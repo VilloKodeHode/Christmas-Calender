70bb5295eae638834c092a5de1085d5436e30b44
Joakim Villo <villokodehode@gmail.com>
2022-12-06 23:49:49 +0100

    added js effect to two "luker"

--- index.js
+++ index.js
@@ -1,9 +1,72 @@
 //! Challange! use Js to make a button for each luke to make an image appear (appendchild/remove child?)
 
-const luke24Text = document.getElementById("luke24Text");
+//* fungerer men er helt ubrukelig:
+const checkBox1 = document.getElementById("inputBox1");
+const checkBox2 = document.getElementById("inputBox2");
+const image = document.getElementsByTagName("img");
+const body = document.getElementById("body");
 
-console.log(luke24Text);
+imageArray = [];
+
+imageArray.push(image);
+
+console.log(image);
+
+// console.log(button);
+// console.log(button2);
 //! kanskje må html flyttes til html.index?
+
+checkBox1.addEventListener("click", () => {
+  if (image[0].style.display === "inline") {
+    image[0].style.display = "none";
+  } else {
+    image[0].style.display = "inline";
+  }
+  console.log(image[0].style.display);
+});
+// button2.addEventListener("click", console.log("click2"));
+//*
+checkBox2.addEventListener("click", () => {
+  if (checkBox2.checked) {
+    image[1].style.transition = "2s ease";
+    setTimeout(() => {
+      image[1].style.transform =
+        "translateY(-500px) translateX(1000px) rotate(40deg)";
+    }, 500);
+    setTimeout(() => {
+      image[1].style.transform = "translateX(-800px) rotate(-90deg)";
+    }, 2000);
+    setTimeout(() => {
+      image[1].style.transform = "translateY(600px) rotate(120deg)";
+    }, 3500);
+    setTimeout(() => {
+      image[1].style.transform = "translateX(700px)";
+    }, 5000);
+    setTimeout(() => {
+      image[1].style.transform = "translateX(500px) translateY(700px)";
+    }, 6500);
+    setTimeout(() => {
+      image[1].style.transform =
+        "translateX(-2000px) translateY(-300px) rotate(500deg)";
+    }, 8000);
+    setTimeout(() => {
+      image[1].style.transform =
+        "translateX(0) translateY(0px) rotate(10800deg)";
+    }, 10000);
+  }
+  console.log(image[1].style.display);
+  console.log(checkBox2.checked);
+});
+
+// <div class="luke jul">
+//   <label>
+//     <input type="checkbox" />
+//     <div class="cover"><p>God Jul<br>alle sammen!</p></div>
+//     <div class="inside">
+//     <img
+//   src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/merry-christmas-wishes-greeting-video-cats-ad-design-template-14114ed0a30602feca713c7187ab2158_screen.jpg?ts=1574593470"
+// />
+//     </div>
 
 // const inputName = document.getElementById("inputName");
 // const nameButton = document.getElementById("nameButton");
@@ -21,24 +84,3 @@ console.log(luke24Text);
 //   content2.style.height = "auto";
 //   content2.style.transition = "width 0.5s ease";
 // }
-
-//* fungerer men er helt ubrukelig:
-
-// luke24Text.addEventListener("click", enlargeImg());
-
-// function enlargeImg() {
-//   console.log("click");
-//   luke24Text.style.fontSize = "1px";
-// }
-
-//*
-
-// <div class="luke jul">
-//   <label>
-//     <input type="checkbox" />
-//     <div class="cover"><p>God Jul<br>alle sammen!</p></div>
-//     <div class="inside">
-//     <img
-//   src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/merry-christmas-wishes-greeting-video-cats-ad-design-template-14114ed0a30602feca713c7187ab2158_screen.jpg?ts=1574593470"
-// />
-//     </div>
